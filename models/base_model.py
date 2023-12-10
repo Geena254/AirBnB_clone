5a1efe816b1464502a9d29f96e0ab9602e21bd69
Georgina Kimani <njokikimani001@gmail.com>
2023-12-10 07:22:43 -0800

    updated task 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,13 +8,14 @@ from datetime import datetime
 class BaseModel:
     """This is the base class from which all other classes will inherit."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """This initializes instance attributes
 
         Args:
             - *args: A list of arguments (not used in this implementation)
             - **kwargs: A dict of key-values arguments
         """
+
         if kwargs is not None and kwargs != {}:  # Check if kwargs is not empty
             for key in kwargs:
                 # Handle special cases for 'created_at' and 'updated_at'
@@ -30,11 +31,11 @@ class BaseModel:
                     # Handle other attributes
                     self.__dict__[key] = kwargs[key]
 
-         else:
+        else:
              # Create a new instance with default values
              self.id = str(uuid.uuid4())
              self.created_at = datetime.now()
-             self.updated-at = datetime.now()
+             self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns the official string representation"""
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -24,6 +24,12 @@ class TestBaseModel_instantiation(unittest.TestCase):
     def test_updated_at_is_public_datetime(self):
         self.assertEqual(datetime, type(BaseModel().updated_at))
 
+    def test_no_args_instantiates(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(BaseModel(), models.storage.all().values())
+
     def test_two_models_unique_ids(self):
         bm1 = BaseModel()
         bm2 = BaseModel()
@@ -52,6 +58,31 @@ class TestBaseModel_instantiation(unittest.TestCase):
         self.assertIn("'id': '123456'", bmstr)
         self.assertIn("'created_at': " + dt_repr, bmstr)
         self.assertIn("'updated_at': " + dt_repr, bmstr)
+
+    def test_args_unused(self):
+        bm = BaseModel(None)
+        self.assertNotIn(None, bm.__dict__.values())
+
+    def test_instantiation_with_kwargs(self):
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        bm = BaseModel(id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(bm.id, "345")
+        self.assertEqual(bm.created_at, dt)
+        self.assertEqual(bm.updated_at, dt)
+
+    def test_instantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            BaseModel(id=None, created_at=None, updated_at=None)
+
+    def test_instantiation_with_args_and_kwargs(self):
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        bm = BaseModel("12", id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(bm.id, "345")
+        self.assertEqual(bm.created_at, dt)
+        self.assertEqual(bm.updated_at, dt)
+
 
 class TestBaseModel_to_dict(unittest.TestCase):
     """Unit tests for testing to_dict method of the BaseModel class."""
